bfdb84b38e68184e4e5d0da2556e58323515c2ff
SampleUser0001 <ittimfn+github@gmail.com>
2022-12-18 17:54:21 +0900

    tmp

---
+++ src/provider/ItemProvider.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from "react";
+import messageDatas from "../static/messages.json";
+
+const ItemContext = createContext();
+export const useItems = () => useContext(ItemContext);
+
+export default function ColorProvider({ children }) {
+    const [items, setMessages] = useState(messageDatas);
+    
+    const onChange = (id) => 
+        setMessages(
+            items.map(item => (item.id === id ? item.checked = !item.checked : item))
+        );
+
+    return (
+        <ItemContext.Provider value={{ items, onChange }}>
+            {children}
+        </ItemContext.Provider>
+    );
+
+}
