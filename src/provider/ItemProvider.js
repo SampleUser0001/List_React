ed39834c20db4dfd91918cdbc6ad5221523e81bf
SampleUser0001 <ittimfn+github@gmail.com>
2022-12-18 18:17:08 +0900

    itemが読み込める

--- src/provider/ItemProvider.js
+++ src/provider/ItemProvider.js
@@ -4,18 +4,19 @@ import messageDatas from "../static/messages.json";
 const ItemContext = createContext();
 export const useItems = () => useContext(ItemContext);
 
-export default function ColorProvider({ children }) {
+export default function ItemProvider({ children }) {
     const [items, setMessages] = useState(messageDatas);
     
-    const onChange = (id) => 
+    const onChange = (id) => {
         setMessages(
             items.map(item => (item.id === id ? item.checked = !item.checked : item))
         );
-
+    }
+    
     return (
         <ItemContext.Provider value={{ items, onChange }}>
             {children}
         </ItemContext.Provider>
     );
-
+    
 }
